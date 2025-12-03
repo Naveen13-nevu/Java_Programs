725e4e3afcb2151c36e780211dfe2be3c7c9b174
Naveen13-nevu <naveenkumar12634@gmail.com>
2025-12-03 20:09:44 +0530

    Lamda Update

--- LambdaExpression/Lam.java
+++ LambdaExpression/Lam.java
@@ -1,17 +1,18 @@
 @FunctionalInterface
-interface Places{
+interface Places
+{
    int add(int i,int j);
-   }
-
-public class Lam {
-    
-public static void main(String []a){
- Places p = (i,j)->
- {
-    return i+j;
- };
- int result = p.add(10,20);
- System.out.println(result);
-
 }
+
+public class Lam 
+{
+    public static void main(String []a){
+        
+        Places p = (i,j) -> {
+            return i + j;
+        };
+        
+        int result = p.add(10,20);
+        System.out.println(result);
+    }
 }
