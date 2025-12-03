59b8115cbaf75474fbb9b866a86bb15b34475991
Naveen13-nevu <naveenkumar12634@gmail.com>
2025-12-03 20:19:53 +0530

    Constructor update

---
+++ Constructor/ParameterizedConstructor.java
@@ -0,0 +1,32 @@
+class Student{
+	String name;
+	int age;
+	//Default constructor 
+	Student(){}
+	//Parameterized constructor
+	Student(String n){
+		name = n;
+		age  = 0;
+	}
+	Student(String n, int a ){
+		name = n;
+		age=a;
+	}
+}
+public class ParameterizedConstructor {
+
+	public static void main(String[] args) {
+		Student s = new Student("Kevin",21);
+		Student s1 = new Student("naveen",22);
+		Student s2 = new Student("praveen",21);
+		Student s4 = new Student("arun",25);
+		
+		System.out.println(s.name+" : "+s.age);
+		System.out.println(s1.name+" : "+s1.age);
+		System.out.println(s2.name+" : "+s2.age);
+		System.out.println(s4.name+" : "+s4.age);
+		
+	}
+
+}
+
