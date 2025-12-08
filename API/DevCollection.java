81a66b8383251129b109b5a900b844fb6e9cc7f1
Naveen13-nevu <naveenkumar12634@gmail.com>
2025-12-08 12:44:59 +0530

    Collection of API

---
+++ API/DevCollection.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DevCollection {
+    public static void main(String[] args) {
+       
+        Collection<Integer>nums = new ArrayList<>();
+        nums.add(6);
+        System.out.println(nums);
+    }
+}
--- Array/Arrays.java
+++ Array/Arrays.java
@@ -1,15 +1,11 @@
 
-
 public class Arrays {
 public static void main(String[] args) {
     
-    // int [] num= new int[5];
-    // num[0]=1;
-    // num[1]=2;
+    int  num [][]= new int[5][2];
+    num[0][1]=1;
 
-    int [] number = {1,2,3,4,5};
-    System.out.println(number[1]);
+    // int [] number = {1,2,3,4,5};
+    System.out.println(num[0][1]);
 }
-
-    
 }
