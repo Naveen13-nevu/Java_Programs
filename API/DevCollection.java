0a19e633c78fe5a3ce5edaa6fc3897e3d4f3a503
Naveen13-nevu <naveenkumar12634@gmail.com>
2025-12-08 12:59:49 +0530

    Collections

--- API/DevCollection.java
+++ API/DevCollection.java
@@ -3,9 +3,14 @@ import java.util.Collection;
 
 public class DevCollection {
     public static void main(String[] args) {
-       
-        Collection<Integer>nums = new ArrayList<>();
+        
+        Collection<Integer>nums = new ArrayList<Integer>();
         nums.add(6);
-        System.out.println(nums);
+        nums.add(1);
+        nums.add(3);
+        
+        for (int n : nums) {
+          System.out.println(n);
+        }
     }
 }
