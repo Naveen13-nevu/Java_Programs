6df11b2771b5152139f91ad41d60475a8b8bc0f7
Naveen13-nevu <naveenkumar12634@gmail.com>
2025-12-19 11:19:30 +0530

    update Odd Even

--- ArrayAdvance/CountEvenAndOddnumber.java
+++ ArrayAdvance/CountEvenAndOddnumber.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class CountEvenAndOddNum {
+public class CountEvenAndOddnumber {
     
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
