8b23dfb668a161b465f179bc1106b51dcd59200c
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-01-16 21:35:15 +0530

    collections

---
+++ Collections/Arrays.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Arrays {
+    public static void main(String[] args) {
+		ArrayList <Integer> ar = new ArrayList<>();
+		ar.add(10);
+		ar.add(20);
+		ar.add(30);
+		ar.add(40);
+		ar.add(50);
+		
+		System.out.println(ar);
+	}
+}
