b0659cacc5242a7666b5c44992338e5815746760
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-01-25 21:49:34 +0530

    Patterns

---
+++ Patterns/InvertedRightAbgleTriangle.java
@@ -0,0 +1,11 @@
+public class InvertedRightAbgleTriangle {
+    public static void main(String[] args) {
+        int n=1;
+        for(int i=5;i>=n;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.err.println();
+        }
+    }
+}
