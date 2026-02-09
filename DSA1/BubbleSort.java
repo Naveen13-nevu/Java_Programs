1d5307474775753c9a14e41001b1af6f784e6247
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-02-09 20:54:44 +0530

    DSA

---
+++ DSA1/BubbleSort.java
@@ -0,0 +1,27 @@
+
+public class BubbleSort {
+    static void bubble(int []arr){
+        int n = arr.length;
+        for(int i =0; i<n-1;i++){
+            for (int j=0;j<n-i-1;j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+
+    }
+    public static void main(String[] args) {
+        
+        int arr[]= {2,5,6,9,1,3,0};
+        bubble(arr);
+        for (int n:arr) {
+            System.out.print(n+" ");
+        }
+    }
+
+
+}
